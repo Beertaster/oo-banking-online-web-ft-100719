9e7c01c62b1ed783aa6808f5ad21dd31179373f0
William Stevens <wstevens.hornet@gmail.com>
2019-10-28 15:52:12 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,12 @@
 class BankAccount
   
-  attr_accessor: :
+  attr_accessor: : :balance :status
+  attr_reader: :name 
+  
+  def initialize(acc_name)
+    @name = acc_name 
+    @balance = 1000 
+    @status = "open"
   
   
 end
